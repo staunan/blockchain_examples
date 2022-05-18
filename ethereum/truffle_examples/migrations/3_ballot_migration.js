41a1624f5b49cb0b394405f7e272a4fd67489df8
Santanu Bera <santanu.bera@computronicsonline.com>
2022-05-18 15:53:37 +0530

    x

--- ethereum/truffle_examples/migrations/3_ballot_migration.js
+++ ethereum/truffle_examples/migrations/3_ballot_migration.js
@@ -1,5 +1,5 @@
 const Ballot = artifacts.require("Ballot");
 
-module.exports = function (deployer) {
-  deployer.deploy(Ballot);
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(Ballot, ['BJP', 'TMC', 'Congress', 'CPIM'], {gas: 9000000, from: accounts[0]});
 };
